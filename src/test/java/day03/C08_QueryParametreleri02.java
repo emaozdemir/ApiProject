3454c7df5ae37e867f8b88f41023b9f6cd965db9
emaozdemir <semaozdemiraslan@gmail.com>
2024-08-02 21:47:16 +0200

    test update

---
+++ src/test/java/day03/C08_QueryParametreleri02.java
@@ -0,0 +1,26 @@
+package day03;
+
+import base_urls.RestFullBookerBaseUrl;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+
+public class C08_QueryParametreleri02 extends RestFullBookerBaseUrl {
+
+
+    @Test
+    public void test01() {
+        //    1. Set the URL
+        spec.pathParam("first", "booking")
+                .queryParam("checkin", "2018-01-01");
+        //    2. Set the expected data
+        //    3. Send the request and get the response
+
+        //    4. Do Assertion
+        given(spec)
+                .when()
+                .get("{first}")
+                .prettyPrint();
+    }
+}
